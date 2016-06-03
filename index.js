8174f513c9838724655635ab8996ed12637ada52
fernandopj <fernando@cellide.com>
2016-06-03 20:20:20 -0300

    Debug what mailgun sends

---
+++ index.js
@@ -0,0 +1,29 @@
+var express            = require('express'),
+	bodyparser         = require('body-parser'),
+	mailgun            = require('mailgun'),
+	http               = require('http');
+
+var app = express();
+var mailgun_server = mailgun({
+	apiKey: process.env.MAILGUN_API_KEY,
+	domain: process.env.MAILGUN_DOMAIN
+});
+
+// create application/json parser
+var jsonparser = bodyparser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedparser = bodyparser.urlencoded();
+
+app.set('port', process.env.PORT || 80);
+app.use(app.router);
+
+app.post('/email-spv', urlencodedparser, parseEmail);
+
+function parseEmail(req, res, next) {
+	console.log(req.body);
+	res.send(200);
+}
+
+var server = http.createServer(app);
+server.listen(process.env.PORT || 80, process.env.IP || '127.0.0.1');
